4cdfe5d5de604891351c49c18fe0bbed7b82e51e
Jacob Joseph <jjoseph03@mica.edu>
2016-04-06 23:25:12 +0000

    created plugin container

---
+++ wp-content/plugins/crc-graduate-nyc-survey-map/crc-graduate-nyc-survey-map.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package crc-graduate-nyc-survey-map
+ * @version 0.1
+ * @author Carson Research Consulting (CRC)
+ */
+/*
+Plugin Name: Graduate NYC - Program Survey Map
+Plugin URI: http://www.carsonresearch.com
+Description: Custom plugin for Graduate NYC program survey map.
+Version: 0.1
+Author: Carson Research Consulting (CRC)
+Author URI: http://www.carsonresearch.com
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+?>
